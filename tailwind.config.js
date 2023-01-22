8817a7a72c535080de5ec2652080a7124fdd78a4
oussamaZAAM <zaam.oussama@gmail.com>
2023-01-23 00:20:24 +0100

    Completing Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-orange':'#FF9351',
+      }
+    },
   },
   plugins: [],
 }

8a1520afc1a4414994c34ca7195cc47e4108b65e
Ayoubon <aarrasseayoub@gmail.com>
2023-02-21 11:36:20 +0100

    add google oauth

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,22 @@
 import "/styles/globals.css";
 import Context from "../context/context";
 import React, { useState, useEffect } from "react";
-export default function App({ Component, pageProps }) {
+import { SessionProvider } from "next-auth/react";
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
     setUser(JSON.parse(localStorage.getItem("user")));
   }, []);
   return (
-    <Context currentUser={user}>
-      <Component {...pageProps} />
-    </Context>
+    <SessionProvider session={session}>
+      <Context currentUser={user}>
+        <Component {...pageProps} />
+      </Context>
+    </SessionProvider>
   );
 }

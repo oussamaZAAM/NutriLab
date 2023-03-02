cc146577194796263a9ebdcff9569b7f1ce5e7ad
Ayoubon <aarrasseayoub@gmail.com>
2023-03-02 10:06:24 +0100

    fixing backend status code

--- pages/api/user.js
+++ pages/api/user.js
@@ -4,16 +4,19 @@ import jwt from "jsonwebtoken";
 export default async function user(req, res) {
   const { cookies } = req;
   const token = cookies.NutriLab;
-  !token && res.status(401).json("Not Logged In");
-  const userr = jwt.verify(token, process.env.JWT_SECRET);
-  try {
-    const user = await prisma.User.findUnique({
-      where: {
-        id: userr.id,
-      },
-    });
-    res.status(200).json(user);
-  } catch (e) {
-    res.status(404).json({ message: "No User Info" });
+  if (token === undefined) {
+    res.status(200).json("Not Logged In");
+  } else {
+    const userr = jwt.verify(token, process.env.JWT_SECRET);
+    try {
+      const user = await prisma.User.findUnique({
+        where: {
+          id: userr.id,
+        },
+      });
+      res.status(200).json(user);
+    } catch (e) {
+      res.status(404).json({ message: "No User Info" });
+    }
   }
 }

986b664515eb0e0a21dc31d3964ea4814200626d
oussamaZAAM <zaam.oussama@gmail.com>
2023-03-03 00:21:13 +0100

    Add Skeleton if profile is not loaded yet

--- pages/profile.js
+++ pages/profile.js
@@ -14,17 +14,17 @@ import ProfilePassword from "../components/ProfilePassword";
 import { User_data } from "../context/context";
 
 const Profile = () => {
-    const { user, setUser } = useContext(User_data);
-    const [page, setPage] = useState(1);
-    const [profile, setProfile] = useState();
+  const { user, setUser } = useContext(User_data);
+  const [page, setPage] = useState(1);
+  const [profile, setProfile] = useState();
 
-    useEffect(()=>{
-        const fetchProfile = async () => {
-            const res = await axios.get('/api/profile');
-            console.log(res)
-        }
-        fetchProfile();
-    }, []);
+  useEffect(() => {
+    const fetchProfile = async () => {
+      const res = await axios.get("/api/profile");
+      setProfile(res.data);
+    };
+    fetchProfile();
+  }, []);
   return (
     <>
       <Head>
@@ -58,14 +58,35 @@ const Profile = () => {
                 </p>
               </div>
             </div>
-            <div className="sticky top-28 left-0 flex flex-col justify-start items-center w-full space-y-2 py-8">
-                <div className="flex h-9 w-10/12 space-x-2 group" onClick={()=>setPage(1)}>
-                    <div className={"self-center border-r-4 border-custom-orange h-5/6 w-2 transition origin-bottom ease-[cubic-bezier(1,-0.4,1,.65)] "+(page===1 ? '-translate-y-0 opacity-100' : 'translate-y-10 opacity-0')}></div>
-                    <div className="
-                                    flex justify-start items-center
-                                    h-full w-full
-                                    cursor-pointer hover:bg-[#635953]
-                                    rounded-lg
+          ) : (
+            <div class="sticky top-0 left-0 flex w-full items-center justify-center space-x-6 bg-[#191919] py-4">
+              <div className="h-20 w-20 animate-pulse rounded-full bg-gray-200"></div>
+              <div class="flex animate-pulse flex-col items-start justify-center">
+                <div class="dark:bg-gray-700 mb-4 h-5 w-40 rounded-full bg-gray-200"></div>
+                <div class="dark:bg-gray-700 mb-2.5 h-3 w-40 max-w-[480px] rounded-full bg-gray-200"></div>
+              </div>
+              <span class="sr-only">Loading...</span>
+            </div>
+          )}
+          <div className="sticky top-28 left-0 flex w-full flex-col items-center justify-start space-y-2 py-8">
+            <div
+              className="group flex h-9 w-10/12 space-x-2"
+              onClick={() => setPage(1)}
+            >
+              <div
+                className={
+                  "h-5/6 w-2 origin-bottom self-center border-r-4 border-custom-orange transition ease-[cubic-bezier(1,-0.4,1,.65)] " +
+                  (page === 1
+                    ? "-translate-y-0 opacity-100"
+                    : "translate-y-10 opacity-0")
+                }
+              ></div>
+              <div
+                className="
+                                    flex h-full w-full
+                                    cursor-pointer items-center
+                                    justify-start rounded-lg
+                                    hover:bg-[#635953]
                                     "
                     >
                         <BiUserCircle className="h-6 w-6 fill-[#C8C8C8] mx-4" />

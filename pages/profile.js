cfc81d99f08a96ce5e830553f794409c6d11e0d1
oussamaZAAM <zaam.oussama@gmail.com>
2023-03-02 19:33:08 +0100

    SideBar sticky on scroll

--- pages/profile.js
+++ pages/profile.js
@@ -29,7 +29,7 @@ const Profile = () => {
       {/* Profile Page  */}
       <div className="grid grid-cols-12">
         <div className="col-span-3 flex flex-col justify-start items-center min-h-screen bg-[#4B4B4B] border-r-4 border-custom-orange">
-            <div className="flex justify-center items-center bg-[#191919] py-4 w-full space-x-6">
+            <div className="sticky top-0 left-0 flex justify-center items-center bg-[#191919] py-4 w-full space-x-6">
                 <Image 
                     width={100}
                     height={100}
@@ -45,7 +45,7 @@ const Profile = () => {
                     </p>
                 </div>
             </div>
-            <div className="flex flex-col justify-start items-center h-full w-full space-y-2 py-8">
+            <div className="sticky top-28 left-0 flex flex-col justify-start items-center w-full space-y-2 py-8">
                 <div className="flex h-9 w-10/12 space-x-2 group" onClick={()=>setPage(1)}>
                     <div className={"self-center border-r-4 border-custom-orange h-5/6 w-2 transition origin-bottom ease-[cubic-bezier(1,-0.4,1,.65)] "+(page===1 ? '-translate-y-0 opacity-100' : 'translate-y-10 opacity-0')}></div>
                     <div className="
@@ -60,20 +60,22 @@ const Profile = () => {
                     </div>
                 </div>
                 <div className="flex h-9 w-10/12 space-x-2 group" onClick={()=>setPage(2)}>
-                <div className={"self-center border-r-4 border-custom-orange h-5/6 w-2 transition origin-center ease-[cubic-bezier(1,-0.4,1,.65)] "+(page===2 ? 'scale-y-100' : 'scale-y-0')}></div>
-                    <div className="
-                                    flex justify-start items-center
-                                    h-full w-full
-                                    cursor-pointer hover:bg-[#635953]
-                                    rounded-lg
-                                    "
-                    >
-                        <IoMdInformationCircleOutline className="h-6 w-6 fill-[#C8C8C8] mx-4" />
-                        <p className="text-sm font-logo text-[#C8C8C8]">Diet informations</p>
-                    </div>
+                    <div className={"self-center border-r-4 border-custom-orange h-5/6 w-2 transition origin-center ease-[cubic-bezier(1,-0.4,1,.65)] "+(page===2 ? 'scale-y-100' : 'scale-y-0')}></div>
+                    <a href="#dietInformations">
+                        <div className="
+                                        flex justify-start items-center
+                                        h-full w-full
+                                        cursor-pointer hover:bg-[#635953]
+                                        rounded-lg
+                                        "
+                        >
+                            <IoMdInformationCircleOutline className="h-6 w-6 fill-[#C8C8C8] mx-4" />
+                            <p className="text-sm font-logo text-[#C8C8C8]">Diet informations</p>
+                        </div>
+                    </a>
                 </div>
                 <div className="flex h-9 w-10/12 space-x-2 group" onClick={()=>setPage(3)}>
-                <div className={"self-center border-r-4 border-custom-orange h-5/6 w-2 transition duration-100 origin-top ease-[cubic-bezier(1,0,1,0)] "+(page===3 ? '-translate-y-0 opacity-100' : '-translate-y-10 opacity-0')}></div>
+                    <div className={"self-center border-r-4 border-custom-orange h-5/6 w-2 transition duration-100 origin-top ease-[cubic-bezier(1,0,1,0)] "+(page===3 ? '-translate-y-0 opacity-100' : '-translate-y-10 opacity-0')}></div>
                     <div className="
                                     flex justify-start items-center
                                     h-full w-full

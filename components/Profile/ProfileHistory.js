61ad2803fcc9325c0f8f754f1fb0305ed7f9ba89
oussamaZAAM <zaam.oussama@gmail.com>
2023-03-14 17:14:48 +0100

    Add Animations to Date Changes

--- components/Profile/ProfileHistory.js
+++ components/Profile/ProfileHistory.js
@@ -2,8 +2,13 @@ import axios from "axios";
 import dayjs from "dayjs";
 import Head from "next/head";
 import { useState, useEffect } from "react";
-import { IoIosArrowDropleftCircle, IoIosArrowDroprightCircle } from "react-icons/io";
+import {
+  IoIosArrowDropleftCircle,
+  IoIosArrowDroprightCircle,
+} from "react-icons/io";
 const ProfileHistory = () => {
+  const [previousAnimation, setPreviousAnimation] = useState(false);
+  const [nextAnimation, setNextAnimation] = useState(false);
   const [foodHistory, setFoodHistory] = useState([]);
   const yesterday = dayjs().add(-1, "days");
   const today = dayjs();
@@ -12,34 +17,43 @@ const ProfileHistory = () => {
   const [previousDate, setPreviousDate] = useState(yesterday);
   const [date, setDate] = useState(today);
   const [nextDate, setNextDate] = useState(tomorrow);
+
   const previousDay = () => {
-    setPreviousDate(prevDate => {
-      const newDate = prevDate.add(-1, 'day');
-      return newDate;
-    })
-    setDate(prevDate => {
-      const newDate = prevDate.add(-1, 'day');
-      return newDate;
-    })
-    setNextDate(prevDate => {
-      const newDate = prevDate.add(-1, 'day');
-      return newDate;
-    })
-  }
+    setPreviousAnimation(true);
+    setTimeout(()=>{
+      setPreviousDate((prevDate) => {
+        const newDate = prevDate.add(-1, "day");
+        return newDate;
+      });
+      setDate((prevDate) => {
+        const newDate = prevDate.add(-1, "day");
+        return newDate;
+      });
+      setNextDate((prevDate) => {
+        const newDate = prevDate.add(-1, "day");
+        return newDate;
+      });
+      setPreviousAnimation(false);
+    }, 90)
+  };
   const nextDay = () => {
-    setPreviousDate(prevDate => {
-      const newDate = prevDate.add(1, 'day');
-      return newDate;
-    })
-    setDate(prevDate => {
-      const newDate = prevDate.add(1, 'day');
-      return newDate;
-    })
-    setNextDate(prevDate => {
-      const newDate = prevDate.add(1, 'day');
-      return newDate;
-    })
-  }
+    setNextAnimation(true);
+    setTimeout(()=>{
+      setPreviousDate((prevDate) => {
+        const newDate = prevDate.add(1, "day");
+        return newDate;
+      });
+      setDate((prevDate) => {
+        const newDate = prevDate.add(1, "day");
+        return newDate;
+      });
+      setNextDate((prevDate) => {
+        const newDate = prevDate.add(1, "day");
+        return newDate;
+      });
+      setNextAnimation(false);
+    }, 90);
+  };
   // useEffect(() => {
   //   async function getHistory() {
   //     const foodHistoryData = await axios.get("/api/foodList");
@@ -56,22 +70,39 @@ const ProfileHistory = () => {
       </Head>
 
       <>
-        <div className="flex justify-center items-center relative mt-2 mb-1 w-full lg:w-2/3">
-          <button><IoIosArrowDropleftCircle className="fill-white h-6 w-6 mx-1 lg:mx-2" onClick={previousDay} /></button>
-          <div className="mx-1 lg:mx-2 h-16 w-14 flex flex-col justify-center items-center rounded-md text-black bg-white font-logo border-2 border-black">
-            <p className="font-bold text-xl">{previousDate.format("DD")}</p>
-            <p className="font-medium text-md">{previousDate.format("MMM")}</p>
+        <div className="relative mt-2 mb-1 flex w-full items-center justify-center lg:w-2/3">
+          <button>
+            <IoIosArrowDropleftCircle
+              className="mx-1 h-6 w-6 fill-white lg:mx-2"
+              onClick={previousDay}
+            />
+          </button>
+          <div className={`mx-1 flex h-16 w-14 flex-col items-center justify-center rounded-md border-2 border-black bg-white font-logo text-black lg:mx-2 `+
+                         (previousAnimation && 'animate-rdate')}>
+            <p className="text-xl font-bold">{previousDate.format("DD")}</p>
+            <p className="text-md font-medium">{previousDate.format("MMM")}</p>
           </div>
-          <div className="mx-1 lg:mx-2 h-24 w-20 flex flex-col justify-center items-center rounded-md text-black bg-white font-logo border-2 border-black">
-            <p className="font-bold text-2xl">{date.format("DD")}</p>
-            <p className="font-medium text-lg">{date.format("MMM")}</p>
+          {/* <div className={`mx-1 flex h-24 w-20 flex-col items-center justify-center rounded-md border-2 border-black bg-white font-logo text-black lg:mx-2 `+
+                         (nextAnimation && 'animate-ldate2 ')+
+                         (previousAnimation && 'animate-rdate2 ')
+                         }> */}
+          <div className={["mx-1 flex h-24 w-20 flex-col items-center justify-center rounded-md border-2 border-black bg-white font-logo text-black lg:mx-2", (nextAnimation && 'animate-ldate2'), (previousAnimation && 'animate-rdate2 ')].join(" ")} >
+                         
+            <p className="text-2xl font-bold">{date.format("DD")}</p>
+            <p className="text-lg font-medium">{date.format("MMM")}</p>
             <p className="font- text-sm">{date.format("YYYY")}</p>
           </div>
-          <div className="mx-1 lg:mx-2 h-16 w-14 flex flex-col justify-center items-center rounded-md text-black bg-white font-logo border-2 border-black">
-            <p className="font-bold text-xl">{nextDate.format("DD")}</p>
-            <p className="font-medium text-md">{nextDate.format("MMM")}</p>
+          <div className={`mx-1 flex h-16 w-14 flex-col items-center justify-center rounded-md border-2 border-black bg-white font-logo text-black lg:mx-2 `+
+                         (nextAnimation && 'animate-ldate')}>
+            <p className="text-xl font-bold">{nextDate.format("DD")}</p>
+            <p className="text-md font-medium">{nextDate.format("MMM")}</p>
           </div>
-          <button><IoIosArrowDroprightCircle className="fill-white h-6 w-6 mx-1 lg:mx-2" onClick={nextDay} /></button>
+          <button>
+            <IoIosArrowDroprightCircle
+              className="mx-1 h-6 w-6 fill-white lg:mx-2"
+              onClick={nextDay}
+            />
+          </button>
         </div>
       </>
     </>
